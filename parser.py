ee603d944ac8df06a6fd3627bfbdebda3fc346ef
Cody Brocious <cody.brocious@gmail.com>
2015-04-27 17:18:04 -0400

    Refactored parser code.  Added string joining support.

--- parser.py
+++ parser.py
@@ -1,33 +1,23 @@
 import grammar
 from grako.ast import AST
 
-def entity(type):
-	def sub(func, *args, **kwargs):
-		def subsub(self, ast, *args, **kwargs):
-			ast['entity'] = type
-			ret = func(self, ast, *args, **kwargs)
-			return ast if ret is None else ret
-		return subsub
-	return sub
-
 class Semantics(object):
 	def _default(self, ast, *args, **kwargs):
 		if isinstance(ast, AST) and 'rule' not in ast:
 			ast['rule'] = ast._parseinfo.rule
 		return ast
 	def tokString(self, ast):
-		return dict(chars=''.join(ast), rule='tokString')
+		return ''.join(ast)
 	def listJoiner(self, ast):
 		return [ast[0]] + ast[1]
+	def stringJoin(self, ast):
+		return dict(chars=''.join(ast), rule='tokString')
 	valueListNE = listJoiner
 	dagArgList = listJoiner
 	templateArgList = listJoiner
 	baseClassListNE = listJoiner
 
-if __name__=='__main__':
-	import sys, pprint
-	with open(sys.argv[1]) as f:
-		text = f.read()
+def parse(text):
 	parser = grammar.grammarParser(parseinfo=True)
 	ast = parser.parse(
 		text,
@@ -42,4 +32,10 @@ if __name__=='__main__':
 			return {k:flatten(v) for k, v in ast.items()}
 		else:
 			return ast
-	pprint.pprint(flatten(ast))
+	return flatten(ast)	
+
+if __name__=='__main__':
+	import sys, pprint
+	with open(sys.argv[1]) as f:
+		text = f.read()
+	pprint.pprint(parse(text))
