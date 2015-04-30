ab63913cdd92c68f6e3b679a17e6d081a0e5e260
Cody Brocious <cody.brocious@gmail.com>
2015-04-30 17:35:17 -0400

    Added grako dep.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
 setup(name='PyTableGen',
-		version='0.1', 
+		version='0.1.1', 
 		description='Python port of LLVM\'s TableGen tool', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
 		packages=['tblgen'], 
 		scripts=['scripts/pytblgen'], 
+		install_requires=[
+			'grako',
+		]
 	)
