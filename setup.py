cd7f3830cf1bf2a2ba013b0453020d96e8bb91fd
Cody Brocious <cody.brocious@gmail.com>
2015-04-30 17:29:18 -0400

    Added setup file, moved dump_defs to scripts/pytblgen.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PyTableGen',
+		version='0.1', 
+		description='Python port of LLVM\'s TableGen tool', 
+		author='Cody Brocious', 
+		author_email='cody.brocious@gmail.com', 
+		packages=['tblgen'], 
+		scripts=['scripts/pytblgen'], 
+	)
---
+++ tblgen/__init__.py
@@ -0,0 +1,4 @@
+from interpreter import interpret, Dag, TableGenType, TableGenList, TableGenBits
+load = interpret
+
+__all__ = 'load interpret Dag TableGenType TableGenList TableGenBits'.split(' ')
