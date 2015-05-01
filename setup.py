d8037d7acbe521c3780bb307193d865d3b28b4ba
Cody Brocious <cody.brocious@gmail.com>
2015-05-01 01:40:35 -0400

    Bumped version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='PyTableGen',
-		version='0.1.1', 
+		version='0.2', 
 		description='Python port of LLVM\'s TableGen tool', 
 		author='Cody Brocious', 
 		author_email='cody.brocious@gmail.com', 
